6a938e9969685f8e3a8f15127ac18c8c2a9a4a80
priteshyadav444 <priteshyadav2015@Gmail.com>
2022-10-19 05:27:48 +0530

    project routes (get all project ) routed

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -14,6 +14,7 @@ const { ObjectId } = require("mongodb");
 router.post("/signup", function (req, res) {
   const { firstname, lastname, password, email } = req.body;
   console.log("v1/users/signup METHOD : POST");
+  
   if (!firstname || !lastname || !email || !password) {
     return res.status(400).json({ msg: "ALL_FIELD_REQUIRED" });
   }
@@ -155,14 +156,14 @@ router.post("/project", authUser, (req, res) => {
   console.log("v1/users/project METHOD : POST");
   const _id = ObjectId();
   const userid = req.user;
-  var { project_title } = req.body;
+  var { project_title,project_deadline } = req.body;
   console.log(project_title);
   console.log(userid)
   if(project_title==null || project_title ==""){
     return res.status(400).json({ msg: "PROJECT_TITLE_REQUIRED" });
   }
   else{
-    const newproject = { project_title , _id }
+    const newproject = { project_title , _id , project_deadline, "total_tasks":0, "total_completed_tasks":0 };
     User.updateOne({ _id: userid }, { $push: { projects: newproject } })
     .then((result) => {
       res.status(200).json(newproject);
@@ -174,4 +175,31 @@ router.post("/project", authUser, (req, res) => {
   }
 
 });
+
+
+// v1/users/project
+// Fetch All Project
+// Private 
+router.get("/project", authUser, (req, res) => {
+  console.log("v1/users/ METHOD : Project");
+  const userid = req.user;
+  const projectid = "634c77fe9b0bdb5860e4e801";
+
+  User.findOne(
+    { _id: userid },
+    function (err, result) {
+      if (err) {
+        res.status(400).json({ msg: "SOMETHING_WENT_WRONG" });
+      }
+
+      if (result) {
+        const projectList = result.projects;
+        return res.status(200).json([...projectList]);
+      }
+    }
+  );
+});
+
+
+
 module.exports = router;
