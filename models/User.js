0e2a540775062d20f5048b5a3157dde69695eb54
priteshyadav444 <priteshyadav2015@Gmail.com>
2022-10-19 05:25:10 +0530

    user model update

--- models/User.js
+++ models/User.js
@@ -1,80 +1,99 @@
 const moongoose = require("mongoose");
-const Schema = moongoose.Schema
+const Schema = moongoose.Schema;
 
 const UserScheme = new Schema(
-    {
-        firstname: { type: String, required: true },
-        lastname: { type: String, required: true },
-        imgurl: { type: String, default: null },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        register_date: { type: Date, default: Date.now() },
-        projects:[{
-            project_title:{ type: String, required: true },
-            tasks: [
-                {
-                    title: { type: String, required: true },
-                    completed:{type:Boolean,default:false},
-                    description: { type: String, default: null },
-                    task_status: { type: String, required: true, default: "pending" },
-                    category: { type: String, default: "None" },
-                    badge:{ type: String, default: "low" },
-                    scheduled_type:{ type: String, required: true, default:"unscheduled_task" },
-                    scheduled_date:{ type: Date, default: null },
-                    subtasklist:[
-                        {
-                            stitle: { type: String,  required: true },
-                            checked:{ type: Boolean, default: false },
-                        }
-                    ],
-                    createdAt: { type: Date, default: Date.now() },
-                    updatedAt: { type: Date, default: Date.now() },
-                }
-            ]
-    }],
-
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    imgurl: { type: String, default: null },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    register_date: { type: Date, default: Date.now() },
+    projects: [
+      {
+        project_title: { type: String, required: true },
+        project_deadline: { type: Date, default: null },
+        total_tasks: { type: Number, default: 0 },
+        total_completed_tasks: { type: Number, default: 0 },
         tasks: [
-            {
-                title: { type: String, required: true },
-                completed:{type:Boolean,default:false},
-                description: { type: String, default: null },
-                task_status: { type: String, required: true, default: "pending" },
-                category: { type: String, default: "None" },
-                badge:{ type: String, default: "low" },
-                scheduled_type:{ type: String, required: true, default:"unscheduled_task" },
-                scheduled_date:{ type: Date, default: null },
-                subtasklist:[
-                    {
-                        stitle: { type: String,  required: true },
-                        checked:{ type: Boolean, default: false },
-                    }
-                ],
-                createdAt: { type: Date, default: Date.now() },
-                updatedAt: { type: Date, default: Date.now() },
-            }
+          {
+            title: { type: String, required: true },
+            completed: { type: Boolean, default: false },
+            description: { type: String, default: null },
+            task_status: { type: String, required: true, default: "pending" },
+            category: { type: String, default: "None" },
+            badge: { type: String, default: "low" },
+            scheduled_type: {
+              type: String,
+              required: true,
+              default: "unscheduled_task",
+            },
+            scheduled_date: { type: Date, default: null },
+            subtasklist: [
+              {
+                stitle: { type: String, required: true },
+                checked: { type: Boolean, default: false },
+              },
+            ],
+            createdAt: { type: Date, default: Date.now() },
+            updatedAt: { type: Date, default: Date.now() },
+          },
         ],
-        completed_task:[
-            {
-                title: { type: String, required: true },
-                completed:{type:Boolean,default:false},
-                description: { type: String, default: null },
-                task_status: { type: String, required: true, default: "pending" },
-                category: { type: String, default: "None" },
-                badge:{ type: String, default: "low" },
-                scheduled_type:{ type: String, required: true, default:"unscheduled_task" },
-            }
-        ],
-        starred:{ type: Boolean, default: false },
-        repeat :{
-            status:{ type: Boolean, default: false },
-            type:{ type: String, default: null },
-            date: { type: Date, default: Date.now() }
-        }
-    },
-    {
-        timestamps: true,
-    }
-)
+        createdAt: { type: Date, default: Date.now() },
+        updatedAt: { type: Date, default: Date.now() },
+      },
+    ],
 
-const User = moongoose.model("User", UserScheme)
+    tasks: [
+      {
+        title: { type: String, required: true },
+        completed: { type: Boolean, default: false },
+        description: { type: String, default: null },
+        task_status: { type: String, required: true, default: "pending" },
+        category: { type: String, default: "None" },
+        badge: { type: String, default: "low" },
+        scheduled_type: {
+          type: String,
+          required: true,
+          default: "unscheduled_task",
+        },
+        scheduled_date: { type: Date, default: null },
+        subtasklist: [
+          {
+            stitle: { type: String, required: true },
+            checked: { type: Boolean, default: false },
+          },
+        ],
+        createdAt: { type: Date, default: Date.now() },
+        updatedAt: { type: Date, default: Date.now() },
+      },
+    ],
+    completed_task: [
+      {
+        title: { type: String, required: true },
+        completed: { type: Boolean, default: false },
+        description: { type: String, default: null },
+        task_status: { type: String, required: true, default: "pending" },
+        category: { type: String, default: "None" },
+        badge: { type: String, default: "low" },
+        scheduled_type: {
+          type: String,
+          required: true,
+          default: "unscheduled_task",
+        },
+      },
+    ],
+    starred: { type: Boolean, default: false },
+    repeat: {
+      status: { type: Boolean, default: false },
+      type: { type: String, default: null },
+      date: { type: Date, default: Date.now() },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = moongoose.model("User", UserScheme);
 module.exports = User;
