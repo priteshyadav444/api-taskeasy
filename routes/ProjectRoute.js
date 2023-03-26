d9b1d34dc3482072893e3e94d356307544412b20
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-03-26 21:32:35 +0530

    project create API updated - validation added - moment added - timezone updated as UTC - express validation added

--- routes/ProjectRoute.js
+++ routes/ProjectRoute.js
@@ -7,10 +7,13 @@ const bcrypt = require("bcryptjs");
 var requestIp = require("request-ip");
 const authUser = require("../middleware/authUser");
 const { ObjectId } = require("mongodb");
+// const { body, validationResult } = require("express-validator");
+const moment = require("moment");
 
 // v1/projects (get)
 // Fetch All Project
 // Private
+
 router.get("/", authUser, (req, res) => {
   console.log("v1/projects/ METHOD : (GET)");
   const userid = req.user;
@@ -30,30 +33,93 @@ router.get("/", authUser, (req, res) => {
 // v1/projects (POST)
 // Create Project
 // PRIVATE
-router.post("/", authUser, (req, res) => {
-  console.log("v1/project METHOD : POST");
-  const _id = ObjectId();
-  const userid = req.user;
-  var { project_title, project_deadline, theme_colour, project_start } =
-    req.body;
+const { body, validationResult } = require("express-validator");
 
-  if (project_start == null || project_start == "") {
-    project_start = new Date();
-  }
-  
-  if (project_title == null || project_title == "") {
-    return res.status(400).json({ msg: "PROJECT_TITLE_REQUIRED" });
-  } else {
+router.post(
+  "/",
+  authUser,
+  [
+    body("project_title")
+      .customSanitizer((value) => value.trim())
+      .isLength({ max: 50 })
+      .withMessage("PROJECT_TITLE_TOO_LONG")
+      .bail()
+      .notEmpty()
+      .withMessage("PROJECT_TITLE_REQUIRED")
+      .bail(),
+    body("project_start")
+      .if(body("project_start").notEmpty())
+      .custom((value, { req }) => {
+        const deadline = moment(value, moment.ISO_8601, true); // parse deadline using ISO 8601 format
+        if (!deadline.isValid()) {
+          throw new Error("INVALID_DATE_FORMAT");
+        }
+        return true;
+      }),
+    body("project_deadline")
+      .notEmpty()
+      .withMessage("PROJECT_DEADLINE_REQUIRED")
+      .bail()
+      .custom((value, { req }) => {
+        const deadline = moment(value, moment.ISO_8601, true); // parse deadline using ISO 8601 format
+        if (!deadline.isValid()) {
+          throw new Error("INVALID_DATE_FORMAT");
+        }
+
+        const deadlineWithoutOffset = moment.utc(
+          deadline.format("YYYY-MM-DDTHH:mm:ss.SSS")
+        ); // remove timezone offset
+        const start = moment(req.body.project_start);
+
+        if (deadlineWithoutOffset.isBefore(start)) {
+          throw new Error("DEADLINE_MUST_BE_GREATER_THAN_START_DATE");
+        }
+
+        return true;
+      }),
+    body("theme_colour")
+      .if(body("project_start").notEmpty())
+      .optional({ nullable: true })
+      .isHexColor()
+      .withMessage("INVALID_THEME_COLOUR"),
+  ],
+  (req, res) => {
+    console.log("v1/project METHOD : POST");
+    const userid = req.user;
+    var { project_title, project_deadline, theme_colour, project_start } =
+      req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    if (project_start == null || project_start == "") {
+      project_start = new moment().utc();
+    }
+
+    // dates configuration
+    project_deadline = moment(project_deadline);
+    project_deadline.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+
+    createdAt = moment.utc();
+    updatedAt = moment.utc();
+
+    // paylaod
+    const _id = ObjectId();
     const newproject = {
-      project_title,
       _id,
-      project_deadline,
-      project_start,
+      project_title,
       theme_colour,
       total_tasks: 0,
       total_completed_tasks: 0,
+      project_deadline,
+      project_start,
     };
-    User.updateOne({ _id: userid }, { $push: { projects: newproject } })
+    User.updateOne(
+      { _id: userid },
+      { $push: { projects: { ...newproject, createdAt, updatedAt } } }
+    )
       .then((result) => {
         res.status(200).json(newproject);
       })
@@ -62,11 +128,12 @@ router.post("/", authUser, (req, res) => {
         return res.status(400).json({ msg: "SOMETHING_WENT_WRONG" });
       });
   }
-});
+);
 
 // v1/projects/123h132g12 (id)
 // Update Project
 // Private
+
 router.put("/:projectId", authUser, (req, res) => {
   console.log("v1/project METHOD : PUT");
   const userid = req.user;
