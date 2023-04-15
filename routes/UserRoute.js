8e336c44258d83f814ca335edca63c1987f8c07c
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-04-16 03:07:49 +0530

    signup and update api updated

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -7,27 +7,31 @@ const bcrypt = require("bcryptjs");
 var requestIp = require("request-ip");
 const authUser = require("../middleware/authUser");
 const { ObjectId } = require("mongodb");
-const { body, validationResult, sanitizeBody } = require("express-validator");
+const { body, validationResult, sanitizeBody, check } = require("express-validator");
 const {
   getErrorPayload,
   getSuccessPayload,
 } = require("../shared/PayloadFormat");
 
-// v1/users/signup
-// Create Account
-// Public
-router.post(
-  "/signup",
+const firstnameValidation = [
   body("firstname")
     .trim()
     .isLength({ min: 1, max: 30 })
     .withMessage("First name must be between 1 and 30 characters")
     .bail(),
+];
+
+// Validation for last name
+const lastnameValidation = [
   body("lastname")
     .trim()
     .isLength({ min: 1, max: 30 })
     .withMessage("Last name must be between 1 and 30 characters")
     .bail(),
+];
+
+// Validation for password
+const passwordValidation = [
   body("password")
     .isLength({ min: 8, max: 40 })
     .withMessage("Password must be between 8 and 40 characters")
@@ -39,6 +43,10 @@ router.post(
       "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
     )
     .bail(),
+];
+
+// Validation for email
+const emailValidation = [
   body("email")
     .isEmail()
     .withMessage("Please enter a valid email address")
@@ -47,83 +55,105 @@ router.post(
     .isLength({ max: 100 })
     .withMessage("Email Address is Too Long")
     .bail(),
-  function (req, res) {
-    const { firstname, lastname, password, email } = req.body;
-    console.log("v1/users/signup METHOD : POST");
+];
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    // senitization of input
-    sanitizeBody("firstname").escape();
-    sanitizeBody("lastname").escape();
-    sanitizeBody("password").escape();
-    sanitizeBody("email").normalizeEmail();
+// Combine all validations
+const signupValidation = [
+  ...firstnameValidation,
+  ...lastnameValidation,
+  ...passwordValidation,
+  ...emailValidation,
+];
 
-    User.findOne({ email })
-      .then((user) => {
-        if (user) {
-          return res
-            .status(409)
-            .json(
-              getErrorPayload(
-                "EMAIL_ALREADY_REGISTERED",
-                "User Already Exist",
-                409
-              )
-            );
-        } else {
-          const newmember = new User({
-            firstname,
-            lastname,
-            email,
-            password,
-          });
+// v1/users/signup
+// Create Account
+// Public
+router.post("/signup", signupValidation, function (req, res) {
+  const { firstname, lastname, password, email } = req.body;
+  console.log("v1/users/signup METHOD : POST");
 
-          //create salt & Hash
-
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newmember.password, salt, (err, hash) => {
-              if (err) throw err;
-              newmember.password = hash;
-              const expiresTimeInSeceond = 172800;
-              newmember
-                .save()
-                .then((member) => {
-                  jwt.sign(
-                    { _id: member._id },
-                    process.env.SECRET_KEY,
-                    { expiresIn: expiresTimeInSeceond },
-                    (err, authToken) => {
-                      if (err) throw err;
-                      res.status(200).json({
-                        authToken,
-                        user: {
-                          _id: member._id,
-                          firstname: member.firstname,
-                          lastname: member.lastname,
-                          email: member.email,
-                        },
-                      });
-                    }
-                  );
-                })
-                .catch((err) => {
-                  return res
-                    .status(400)
-                    .json(getErrorPayload("SIGNUP_FAILED", 400));
-                });
-            });
-          });
-        }
-      })
-      .catch((err) => {
-        if (err)
-          res.status(400).json(getErrorPayload("Something went Wrong", 400));
-      });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
+  // senitization of input
+  check("firstname").escape();
+  check("lastname").escape();
+  check("password").escape();
+  check("email").normalizeEmail();
+
+  // checking is already exists
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        return res
+          .status(409)
+          .json(
+            getErrorPayload(
+              "EMAIL_ALREADY_REGISTERED",
+              "User Already Exist",
+              409
+            )
+          );
+      } else {
+        const newmember = new User({
+          firstname,
+          lastname,
+          email,
+          password,
+        });
+
+        //create salt & Hash
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newmember.password, salt, (err, hash) => {
+            if (err) throw err;
+            newmember.password = hash;
+            // 2 days expiry time
+            const expiresTimeInSeceond = 172800;
+            newmember
+              .save()
+              .then((member) => {
+                jwt.sign(
+                  { _id: member._id },
+                  process.env.SECRET_KEY,
+                  { expiresIn: expiresTimeInSeceond },
+                  (err, authToken) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                      authToken,
+                      user: {
+                        _id: member._id,
+                        firstname: member.firstname,
+                        lastname: member.lastname,
+                        email: member.email,
+                      },
+                    });
+                  }
+                );
+              })
+              .catch((err) => {
+                return res
+                  .status(400)
+                  .json(
+                    getErrorPayload(
+                      "SIGNUP_FAILED",
+                      "Signup Failed Please Try Again",
+                      400,
+                      err
+                    )
+                  );
+              });
+          });
+        });
+      }
+    })
+    .catch((err) => {
+      if (err)
+        return res
+          .status(400)
+          .json(getErrorPayload("SERVER_ERROR", "Something Went Wrong", 400));
+    });
+});
 
 // v1/users/signin
 // Login Account
@@ -201,89 +231,101 @@ router.get("/load", authUser, (req, res) => {
     });
 });
 
+const updateProfileValidation = [
+  ...firstnameValidation,
+  ...lastnameValidation,
+  ...emailValidation,
+];
+
 // v1/users/updateProfile
 // Update User Profile
 // Private
 router.put(
   "/updateProfile",
   authUser,
-  [
-    body("email")
-      .if(body("email").notEmpty())
-      .isEmail()
-      .withMessage("Enter Valid Email Address")
-      .bail()
-      .customSanitizer((value) => value.trim())
-      .isLength({ max: 100 })
-      .withMessage("EMAIL_TOO_LONG")
-      .bail(),
-  ],
-  function (req, res) {
-    const { firstname, lastname, imgurl, email } = req.body;
+  updateProfileValidation,
+  async function (req, res) {
+    const { firstname, lastname, imgurl, email, country, phone_no } = req.body;
     console.log("v1/users/updateProfile METHOD : PUT");
-
+    // return errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.findById(req.user._id)
-      .then((user) => {
-        // If user not Found
-        if (!user) {
-          return res
-            .status(404)
-            .json(getErrorPayload("USER_NOT_FOUND", 404, error));
-        } else {
-          // If Email Passed for Update. than checking Is Already Exist In A System
-          if (email && email !== user.email) {
-            User.findOne({ email }).then((existingUser) => {
-              if (existingUser) {
-                return res
-                  .status(400)
-                  .json(getErrorPayload("EMAIL_ALREADY_REGISTERED", 400));
-              } else {
-                user.email = email;
-              }
-            });
-          }
-          if (firstname) {
-            user.firstname = firstname;
-          }
+    // Sanitize input fields
+    check("firstname").escape();
+    check("lastname").escape();
+    check("country").escape();
+    check("phone_no").escape();
+    check("email").normalizeEmail();
 
-          if (lastname) {
-            user.lastname = lastname;
-          }
-
-          if (imgurl) {
-            user.imgurl = imgurl;
-          }
-
-          user
-            .save()
-            .then((updatedUser) => {
-              res.status(200).json({
-                user: {
-                  _id: updatedUser._id,
-                  firstname: updatedUser.firstname,
-                  lastname: updatedUser.lastname,
-                  imgurl: updatedUser.imgurl,
-                  email: updatedUser.email,
-                },
-              });
-            })
-            .catch((err) => {
-              return res
-                .status(400)
-                .json(getErrorPayload("PROFILE_UPDATE_FAILED", 400, err));
-            });
-        }
-      })
-      .catch((err) => {
+    try {
+      // Check if user exists
+      const user = await User.findById(req.user._id);
+      if (!user) {
         return res
-          .status(400)
-          .json(getErrorPayload("SOMETHING_WENT_WRONG", 400, err));
+          .status(404)
+          .json(getErrorPayload("USER_NOT_FOUND", "User Not Exists", 404));
+      }
+
+      // If email is passed for update, check if it already exists in the system
+      if (email && email !== user.email) {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return res
+            .status(409)
+            .json(
+              getErrorPayload(
+                "EMAIL_ALREADY_REGISTERED",
+                "Email is already registered",
+                409
+              )
+            );
+        }
+        user.email = email;
+      }
+
+      // Update other fields if they are passed in the request body
+      if (firstname) {
+        user.firstname = firstname;
+      }
+      if (lastname) {
+        user.lastname = lastname;
+      }
+      if (imgurl) {
+        user.imgurl = imgurl;
+      }
+      if (country) {
+        user.country = country;
+      }
+      if (phone_no) {
+        user.phone_no = phone_no;
+      }
+
+      // Save updated user
+      const updatedUser = await user.save();
+
+      // Return updated user details
+      res.status(200).json({
+        user: {
+          _id: updatedUser._id,
+          firstname: updatedUser.firstname,
+          lastname: updatedUser.lastname,
+          imgurl: updatedUser.imgurl,
+          email: updatedUser.email,
+          country: updatedUser.country,
+          phone_no: updatedUser.phone_no,
+        },
       });
+    } catch (error) {
+      // Handle errors
+      return res
+        .status(400)
+        .json(
+          getErrorPayload("SERVER_ERROR", "Something Went Wrong", 400, error)
+        );
+    }
   }
 );
 
