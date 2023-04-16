d968e1f9c111c6b1809a046dbe01e9851d83a7e0
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-04-16 16:55:59 +0530

    middl ware updated

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -5,9 +5,14 @@ const { getErrorPayload } = require("../shared/PayloadFormat");
 
 // user and token
 function authUser(req, res, next) {
-  const token = req.header("x-auth-token");
-  if (!token) return res.status(401).json(getErrorPayload("AUTH_DENAID", "Authentication Failed", 401));
   try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res
+        .status(401)
+        .json(getErrorPayload("AUTH_DENAID", "Authentication Failed. Token Required", 401));
+    }
+    console.log("inside");
     //verify token
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
@@ -19,9 +24,22 @@ function authUser(req, res, next) {
     next();
   } catch (error) {
     if (error instanceof jwt.TokenExpiredError) {
-      return res.status(400).json(getErrorPayload("TOKEN_EXPIRED", "Your Session Expired", 400, error));
+      return res
+        .status(400)
+        .json(
+          getErrorPayload("TOKEN_EXPIRED", "Your Session Expired", 400, error)
+        );
     } else {
-      return res.status(400).json(getErrorPayload("INVALID_TOKEN", "Invalid Token Please Login Again",400, error));
+      return res
+        .status(400)
+        .json(
+          getErrorPayload(
+            "INVALID_TOKEN",
+            "Invalid Token Please Login Again",
+            400,
+            error
+          )
+        );
     }
   }
 }
