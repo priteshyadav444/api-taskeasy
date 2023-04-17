9069ce0b4cf87dd2e8df3078397fd5cc4901d954
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-04-17 23:41:53 +0530

    Schema Updated - deleted projects

--- models/User.js
+++ models/User.js
@@ -7,17 +7,14 @@ const UserScheme = new Schema(
     lastname: { type: String, required: true },
     imgurl: { type: String, default: null },
     country: { type: String, default: null },
-    phone_no: { type: String, default:null },
+    phone_no: { type: String, default: null },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     register_date: { type: Date, default: Date.now() },
     projects: [
       {
         project_title: { type: String, required: true },
-        total_tasks: { type: Number, default: 0 },
-        total_completed_tasks: { type: Number, default: 0 },
         theme_colour: { type: String, default: "#D2DAFF" },
-        
         project_start: { type: Date, default: Date.now() },
         project_deadline: { type: Date, default: null },
         startedAt: { type: Date, default: null },
@@ -25,6 +22,56 @@ const UserScheme = new Schema(
         createdAt: { type: Date, default: Date.now() },
         updatedAt: { type: Date, default: Date.now() },
 
+        tasks: [
+          {
+            title: { type: String, required: true },
+            completed: { type: Boolean, default: false },
+            description: { type: String, default: null },
+            task_status: { type: String, required: true, default: "pending" },
+            badge: { type: String, default: "low" },
+            theme_colour: { type: String, default: "#D2DAFF" },
+            scheduled_date: { type: Date, default: null },
+            createdAt: { type: Date, default: Date.now() },
+            subtasklist: [
+              {
+                stitle: { type: String, required: true },
+                checked: { type: Boolean, default: false },
+              },
+            ],
+          },
+        ],
+        deleted_tasks: [
+          {
+            title: { type: String, required: true },
+            completed: { type: Boolean, default: false },
+            description: { type: String, default: null },
+            task_status: { type: String, required: true, default: "pending" },
+            badge: { type: String, default: "low" },
+            theme_colour: { type: String, default: "#D2DAFF" },
+            scheduled_date: { type: Date, default: null },
+            createdAt: { type: Date, default: Date.now() },
+            subtasklist: [
+              {
+                stitle: { type: String, required: true },
+                checked: { type: Boolean, default: false },
+              },
+            ],
+            deletedAt: { type: Date, default: Date.now() },
+          },
+        ],
+      },
+    ],
+    deleted_projects: [
+      {
+        project_title: { type: String, required: true },
+        theme_colour: { type: String, default: "#D2DAFF" },
+        project_start: { type: Date, default: Date.now() },
+        project_deadline: { type: Date, default: null },
+        startedAt: { type: Date, default: null },
+        completedAt: { type: Date, default: null },
+        createdAt: { type: Date, default: Date.now() },
+        updatedAt: { type: Date, default: Date.now() },
+        deletedAt: { type: Date, default: null },
         tasks: [
           {
             title: { type: String, required: true },
